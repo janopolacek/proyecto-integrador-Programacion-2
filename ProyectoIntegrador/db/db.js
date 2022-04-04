ca930aed1e911dc523c6702032d030b72f153944
gonzaloelia <gelia@udesa.edu.ar>
2022-04-04 20:53:56 -0300

    Creamos archivos db

---
+++ ProyectoIntegrador/db/db.js
@@ -0,0 +1,33 @@
+const alfajores = {
+    lista:  [
+    {
+        nombre: "Terrabusi clasico" ,
+        descripcion: " Alfajor sabor dulce de leche de 6 unidades. Una explosión de sabor en tu boca, un amor para tu paladar",
+        imagen:"",
+        fechaDeCarga:"04/04/2022",
+        precio: "$360",
+    },
+    {
+        nombre: "Mini torta Aguila" ,
+        descripcion: "Sabor brownie de 74 gramos",
+        imagen:"",
+        fechaDeCarga:"04/04/2022",
+        precio: "$154",
+    },
+    {
+        nombre: "Triple Tofi" ,
+        descripcion: "Negro con dulce de leche de 73 gramos",
+        imagen:"",
+        fechaDeCarga:"04/04/2022",
+        precio: "$124",
+    },
+    {
+        nombre: "Caja de alfajores Guauymallen",
+        descripcion:"40 unidades de puro placer sabor membrillo",
+        imagen:"",
+        fechaDeCarga:"",
+        precio:"$799",
+    },
+
+            ]
+}
