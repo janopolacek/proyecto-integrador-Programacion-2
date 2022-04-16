73d30149bdf0e311f4e84c940a56d47ce5f3ff4a
gonzaloelia <gelia@udesa.edu.ar>
2022-04-16 20:16:50 -0300

    arreglos del index.ejs

--- ProyectoIntegrador/controller/indexController.js
+++ ProyectoIntegrador/controller/indexController.js
@@ -3,7 +3,7 @@ const data = require('../db/db')
 const indexControlador = {
     lista: function (req, res) {
         return res.render('index', {
-            listadoAlfajores: data.data.alfajores,
+            listadoAlfajores: data.alfajores,
         })
     },
     register: function (req,res) {
--- ProyectoIntegrador/routes/index.js
+++ ProyectoIntegrador/routes/index.js
@@ -1,5 +1,5 @@
-const express = require ('express')
-const router = express.router
+const express = require ('express');
+const router = express.Router();
 const indexController = require ('../controller/indexController')
 
 router.get ('/', indexController)
