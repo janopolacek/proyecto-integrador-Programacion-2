07e418a59e23310dfc19ae3de54ab197c655bf84
jano polacek <jpolacek@udesa.edu.ar>
2022-06-23 00:16:54 -0300

    profile detail

--- proyectoDeVerdad/ProyectoFinal/controller/profileController.js
+++ proyectoDeVerdad/ProyectoFinal/controller/profileController.js
@@ -1,11 +1,50 @@
 const data = require('../database/models')
+const productos = data.Producto;
+const users = data.Users;
+const comentarios = data.Comment;
+const Follower = data.Followers;
+
+
 
 const profileController = {
-    index: function(req, res) {
-        return res.render('profile',{
-            data: data.usuario,
-            productos: data.producto,
-        })
+    index: function (req, res) {
+        productos.findAll({
+                where: [{
+                    UsersId: req.params.id
+                }],
+            })
+            .then(function (productos) {
+                users.findByPk(req.params.id)
+                    .then(function (usuarios) {
+                        Follower.findAll({
+                                where: [{
+                                    UsersId: req.params.id
+                                }]
+                            })
+                            .then(function (Follower) {
+                                comentarios.findAll({
+                                        include: [{
+                                            association: "UsersComments"
+                                        }],
+                                        where: [{
+                                            UsersId: req.params.id
+                                        }]
+                                    })
+                                    .then(function (comentarios) {
+                                        return res.render('profile', {
+                                            usuarios: usuarios,
+                                            productos: productos,
+                                            Follower: Follower,
+                                            comentarios: comentarios
+                                        });
+                                    })
+                                    .catch(error => console.log(error))
+                            })
+                            .catch(error => console.log(error))
+                    })
+                    .catch(error => console.log(error))
+            })
+            .catch(error => console.log(error))
     },
     edit: function(req, res) {
     return res.render('profileEdit');
--- proyectoDeVerdad/ProyectoFinal/routes/profile.js
+++ proyectoDeVerdad/ProyectoFinal/routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const profileController = require('../controller/profileController')
 
 /* GET home page. */
-router.get('/', profileController.index);
+router.get('/detail/:id', profileController.index);
 
 router.get('/edit', profileController.edit);
 
